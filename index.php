4b41e08d50db8899466bcbe43cb232079a1d5129
QiRoLJoKeR <63529066+QiRoLJoKeR@users.noreply.github.com>
2020-04-12 09:48:24 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  *Telegram : @TILON
  */
-$API_KEY = '1191442541:AAGtoHhhxZzklWjOxQMgyFPVbiqRTPjjFU4';
+$API_KEY = 'token';
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
